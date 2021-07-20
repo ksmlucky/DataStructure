99b48c597151732ecc3f9bf83f373f928363f7ad
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-07-20 19:45:33 +0900

    이중 연결 리스트 구현

---
+++ MyDoubleLinkedList.java
@@ -0,0 +1,125 @@
+public class MyDoubleLinkedList<T> {
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    public class Node<T> {
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public void addNode(T data) {
+        if (this.head == null) {
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public boolean insertToFront(T existedData, T addData) {
+        if (this.head == null) {
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+            return true;
+        } else if (this.head.data == existedData) {
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head;
+            this.head = newHead;
+            return true;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == existedData) {
+                    Node<T> nodePrev = node.prev;
+
+                    nodePrev.next = new Node<T>(addData);
+                    nodePrev.next.next = node;
+
+                    nodePrev.next.prev = nodePrev;
+                    node.prev = nodePrev.next;
+
+                    return true;
+                } else {
+                    node = node.next;
+                }
+            }
+            return false;
+        }
+    }
+
+    public void printAll() {
+        if (this.head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+
+            }
+        }
+    }
+
+    public T searchFromHead(T isData) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public T searchFromTail(T isData) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.tail;
+            while (node != null) {
+                if (node.data == isData) {
+                    return node.data;
+                } else {
+                    node = node.prev;
+                }
+            }
+            return null;
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        MyDoubleLinkedList<Integer> mdll = new MyDoubleLinkedList<Integer>();
+
+        mdll.addNode(2);
+        mdll.addNode(4);
+        mdll.addNode(5);
+        mdll.addNode(8);
+        mdll.addNode(3);
+
+        mdll.printAll();
+
+        System.out.println();
+
+        System.out.println(mdll.searchFromHead(5));
+        System.out.println(mdll.searchFromTail(1));
+    }
+}
