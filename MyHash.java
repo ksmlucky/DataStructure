a68ab9441beff5305aebf2dc901c5b3be8d8087c
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-07-22 20:54:20 +0900

    해쉬 테이블 구현

---
+++ MyHash.java
@@ -0,0 +1,48 @@
+public class MyHash {
+    public Slot[] hashTable;
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String value;
+        Slot(String value) {
+            this.value = value;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            this.hashTable[address].value = value;
+        } else {
+            this.hashTable[address] = new Slot(value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            return this.hashTable[address].value;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyHash mainObject = new MyHash(20);
+
+        mainObject.saveData("DaveLee", "01022223333");
+        mainObject.saveData("Dave", "01055556666");
+        mainObject.saveData("David", "01044445555");
+
+        System.out.println(mainObject.getData("DaveLee"));
+
+    }
+}
